c6b742c6f928cd78c2ebc86e83b8dfc8630901db
apc <alexcurran.va@gmail.com>
2020-06-05 11:24:47 -0500

    Added Router and updated AnimalInfo/AnimalList to include useEffect hooks for API data

--- src/components/AnimalInfo.js
+++ src/components/AnimalInfo.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
-export default function AnimalInfo() {
-    const [animalInfo, setAnimalInfo] = useState([]);
+export default function AnimalInfo({ match }) {
+    const [animalInfo, setAnimalInfo] = useState({});
+    const API_URL = `http://localhost:5000/api/animals/animal/${match.params.id}`;
 
     useEffect(() => {
-        
+        fetch(API_URL)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setAnimalInfo(data);
+            })
+            .catch(err => console.error(err));
     }, []);
 
     return (
         <main className="animal-info-page">
-            <h1>Animal Info Page</h1>
+            <h1>{animalInfo.animal_common_name}</h1>
+            <h2>{animalInfo.animal_scientific_name}</h2>
+            <figure>
+                <img src={animalInfo.main_img_url} alt={animalInfo.animal_common_name}/>
+            </figure>
         </main>
     )
 }

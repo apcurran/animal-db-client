b4c4f4d34917c58e3b9a50ac234891c6f64af133
apc <alexcurran.va@gmail.com>
2020-06-09 11:40:37 -0500

    Updated footer for citation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,9 @@ export default function Footer() {
     return (
         <footer>
             <h3 className="footer__title">&copy; Animal DB 2020</h3>
+            <h4 className="footer__cite">
+                All facts borrowed from <a href="https://www.nationalgeographic.com/animals/" target="_blank" className="footer__cite__link">National Geographic</a>
+            </h4>
         </footer>
     )
 }

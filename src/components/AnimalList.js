05a99307fb4f1582710c4a699b13fdec2a113b17
apc <alexcurran.va@gmail.com>
2020-06-10 13:13:55 -0500

    Added loading indicator on fetch req for animal list

--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 
 export default function AnimalList() {
     const [animals, setAnimals] = useState([]);
+    const [loading, setLoading] = useState(false);
     const API_URL = "https://apcurran-animaldb.herokuapp.com/api/animals";
 
     useEffect(() => {
+        setLoading(true);
+
         fetch(API_URL)
             .then(response => response.json())
             .then(data => {
                 setAnimals(data);
+                setLoading(false);
             })
             .catch(err => console.error(err));
     }, [API_URL]);
@@ -18,6 +22,9 @@ export default function AnimalList() {
         <main className="main">
             <h1 className="main__title">Featured Animals</h1>
             <div className="animal-card-group">
+                {loading && (
+                    <h3 className="loading">Loading...</h3>
+                )}
                 {animals.map(animal => (
                     <article className="animal-card" key={animal._id}>
                         <Link to={`/animal/${animal._id}`}>
